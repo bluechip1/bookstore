699756378eb2798c1bedeb194e4fee7fa13c6625
Kevin Smith <smith.ks64@gmail.com>
2014-04-08 18:57:31 -0400

    lab 2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,18 +14,26 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.save
-    redirect_to @book
+      @book = Book.new(book_params)
+      if @book.save
+        redirect_to @book
+      else
+        render :new
+      end
   end
+
 
   def edit
   end
 
   def update
-    @book.update(book_params)
-    redirect_to @book
+      if @book.update(book_params)
+        redirect_to @book
+      else
+        render :new
+      end
   end
+
 
   def destroy
     @book.destroy
