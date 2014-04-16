434ed5f0c33bd76b9ce4af972062d7b5e7a73567
Kevin Smith <smith.ks64@gmail.com>
2014-04-15 20:07:50 -0400

    lab5

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,6 +11,10 @@ class Book < ActiveRecord::Base
 	    if: "price.present?"
 
 	def average_stars
-	  reviews.average(:stars)
+	  if reviews.loaded?
+	    reviews.map(&:stars).compact.average
+	  else
+	    reviews.average(:stars)
+	  end
 	end
 end
